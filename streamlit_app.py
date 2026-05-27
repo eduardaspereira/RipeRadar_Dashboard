dfe7cdea3d8a39f21ffafeccf5c4a923e7029c9d
Eduarda Pereira <88736790+eduardaspereira@users.noreply.github.com>
2026-05-27 22:38:15 +0100

    rfid 3.1

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,7 @@ import extra_streamlit_components as stx
 st.set_page_config(page_title="RipeRadar OS", page_icon="🍎", layout="wide")
 
 # --- 2. GESTOR DE COOKIES ---
-# Usamos cache para o Streamlit não reiniciar o gestor a cada clique
-@st.cache_resource(experimental_allow_widgets=True)
+@st.cache_resource
 def get_manager():
     return stx.CookieManager()
 
